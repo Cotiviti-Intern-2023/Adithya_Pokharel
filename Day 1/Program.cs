42e246098428a055f351eea45a1924b7931bc39f
adithya <adithya.pokharel@cotiviti.com>
2023-05-23 21:59:42 +0545

    Day 1 and Day 2 added

---
+++ Day 1/Program.cs
@@ -0,0 +1,50 @@
+﻿﻿namespace day_1
+{
+    class day_1{
+        static void Main(){
+            double current_balance=0.0;
+            double amount=0.0;
+            for(int i=0;i<5;i++){
+                Console.WriteLine("w:Withdraw Balance");
+                Console.WriteLine("d:Deposit Balance");
+                Console.WriteLine("c:Check Balance");
+                char choice=Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch(choice){
+                    case 'w':
+                        Console.WriteLine("Enter amount to withdraw.");
+                        amount=Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine();
+                        if(amount<0){
+                            Console.WriteLine("Amount must be greater than 0");
+                        }
+                        else if(current_balance<amount){
+                            Console.WriteLine("Available balance is less than the desired amount.");
+                        }
+                        else{
+                            current_balance=current_balance-amount;
+                            Console.WriteLine($"{amount} is successfully withdrawn remaining balance is {current_balance}");
+                     }
+                        break;
+                    case 'd':
+                        Console.WriteLine("Enter amount to deposit.");
+                        amount=Convert.ToDouble(Console.ReadLine());
+                        if(amount<0){
+                            Console.WriteLine("Deposit amount can't be a negative number.");    
+                        }
+                        else{
+                            current_balance=current_balance+amount;
+                            Console.WriteLine($"{amount} deposited successfully. Current balance is {current_balance}.");
+                        }
+                        break;
+                    case 'c':
+                        Console.WriteLine($"Current balance is {current_balance}.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Day 2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace day_2
+{
+    class day_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter D.O.B in dd/mm/yyyy format:");
+            string in_dob = Console.ReadLine();
+            DateTime out_dob;
+            string[] parts=in_dob.Split('/');
+            string day=parts[0];
+            string month=parts[1];
+            string year=parts[2];
+
+            if(DateTime.TryParse($"{month}/{day}/{year}", out out_dob))
+            {
+                if (DateTime.Now<out_dob)
+                {
+                    Console.WriteLine("Invalid DOB.");
+                }
+                else
+                {
+                    // Calculate age
+                    TimeSpan age = DateTime.Now - out_dob;
+                    int age_year = (int)(age.TotalDays / 365);
+                    int age_month=(int)((age.TotalDays%365)/30);
+                    int age_day=(int)((age.TotalDays%365)%30)%30;
+                    Console.WriteLine($"{age_year} years {age_month} month and {age_day} days");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid date input.");
+            }
+        }
+    }
+}
